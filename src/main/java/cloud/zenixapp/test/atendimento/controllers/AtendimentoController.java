99566f2719d75046dfa525c0c70168c4511ec5d4
Thalles-Ribeiro-Nascimento <thalles.nascimentorj@gmail.com>
2025-12-19 19:24:51 -0300

    Implementacao do Controller

---
+++ src/main/java/cloud/zenixapp/test/atendimento/controllers/AtendimentoController.java
@@ -0,0 +1,23 @@
+package cloud.zenixapp.test.atendimento.controllers;
+
+
+import cloud.zenixapp.test.atendimento.entities.Atendimento;
+import cloud.zenixapp.test.atendimento.services.AtendimentoService;
+import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping(value = "/atendimento")
+public class AtendimentoController {
+
+    @Autowired
+    private AtendimentoService atendimentoService;
+
+    @PostMapping
+    public ResponseEntity<@NonNull Atendimento> save(@RequestBody Atendimento atendimento){
+        return ResponseEntity.ok().body(atendimento);
+    }
+
+}
