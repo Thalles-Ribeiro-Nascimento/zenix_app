3f8691f099d15c371c66e6b5dbd967275f286efa
Thalles-Ribeiro-Nascimento <thalles.nascimentorj@gmail.com>
2025-12-24 06:40:00 -0300

    Implementando o Serializable na classe Atendimento

--- src/main/java/cloud/zenixapp/test/zenix/entities/Atendimento.java
+++ src/main/java/cloud/zenixapp/test/zenix/entities/Atendimento.java
@@ -6,12 +6,18 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
+import java.io.Serializable;
+
 @Entity
 @Table(name = "atendimento")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class Atendimento {
+public class Atendimento implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
